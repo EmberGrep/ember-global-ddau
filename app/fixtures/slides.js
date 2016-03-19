515025385fcaeb1c1fc923dcdb2bf9f84ad2eb3f
Ryan Tablada <ryan.tablada@gmail.com>
2016-03-18 22:50:16 -0500

    Start basic procedure

---
+++ app/fixtures/slides.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    routeName: `introduction`,
+    color: `#76ab59`,
+  },
+  {
+    routeName: `work`,
+    color: `#ec9156`,
+  },
+  {
+    routeName: `newForm`,
+    color:`#5662d9`,
+  }
+];
